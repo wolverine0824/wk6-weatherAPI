9e451d383403bf2587b5f41b047b85fb57f23460
logon pierce <logonpierce@gmail.com>
2021-10-02 00:13:59 -0400

    update working code

--- Assets/index.js
+++ Assets/index.js
@@ -1,12 +1,10 @@
-  const weather = {
-    apiKey: "8854f12c8ed8e64ffd06b1b0e0c9f7be",
-    fetchWeather: function (city) {
-      fetch(
-        "https://api.openweathermap.org/data/2.5/weather?q=" 
-          + city +
-          "&units=metric&appid=" 
-          + this.apiKey
-      )
+const weather = {
+  // this property contains my apiKey with the weather 
+    apiKey: `8854f12c8ed8e64ffd06b1b0e0c9f7be`,
+    // this function fetches the weather data through my api key
+      fetchWeather: function (city) {
+      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${this.apiKey}`) 
+      // this attaches callback for the resolution and or rejection of my promise
          .then((response) => {
            if (!response.ok) {
              alert("No weather found.");
@@ -14,41 +12,38 @@
           }
            return response.json();
          })
-         .then((data) => this.displayWeather(data));
+         .then((data) => this.weather(data));
     },
-
-    displayWeather: function ( data ) {
+// this is the displaying data for the weather information
+    weather: function ( data ) {
         const { name } = data;
-        const { icon, description } = data.weather;
+        const { icon, description } = data.weather[0];
         const { temp, humidity } = data.main;
         const { speed } = data.wind;
+        // her im console logging the weather data
         console.log( name, icon, description, temp, humidity, speed );
-        document.querySelector(".city").innerText = " weather in " + name;
-        document.querySelector(".icon").src =
-            "https://openweathermap.org/img/wn/" + icon + ".png";
+        // returns the first Element within the document that matches the specified selector, or group of selectors
+        document.querySelector(".city").innerText =`weather in${name}`;
+        document.querySelector(".icon").src =`https://openweathermap.org/img/wn/${icon}.png`;
         document.querySelector(".description").innerText = description;
-        document.querySelector(".temp").innerText = temp + "°c";
-        document.querySelector(".humidity").innerText = "humidity: " + humidity + "°c";
-        document.querySelector(".wind").innerText = "wind speed: " + speed + "km/h";
-        document.querySelector(".weather").classList.remove("loading");
-        document.body.style.backgroundImage =
-        url("https://source.unsplash.com/user/erondu/1600x900/?landscape");
+        document.querySelector(".temp").innerText = `temp:°c`;
+        document.querySelector(".humidity").innerText =`humidity:${humidity}°c`;
+        document.querySelector(".wind").innerText =`windspeed:${speed}km/h`;
+        document.querySelector(".weather").classLast.remove("loading");
     },
-
+// this function returns a reference to the first object with the specified value of the ID
     search: function() {
-       this.fetchWeather(document.querySelector(".search-box").value); 
+       var w = this.fetchWeather(document.getElementById("search").value); 
+       console.log(w);
+       return w;
     },
 };
 
-document.querySelector(".search-box").addEventListener("click", function () {
-  });
+
   
   document
-    .querySelector(".search-box")
-    .addEventListener("keyup", function (event) {
-      if (event.key == "Enter") {
-        weather.search();
-      }
+    .querySelector("#btn")
+    .addEventListener("click", function () {
+        weather.search();  
     });
-
-  weather.fetchWeather("Atlanta");
+    
